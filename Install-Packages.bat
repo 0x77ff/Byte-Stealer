0f35fb08442f0d476ea65b4d0194b69638cc9bee
TurtlesXD <116833814+TurtlesXD@users.noreply.github.com>
2023-09-01 08:33:05 +0100

    Update Install-Packages.bat

--- Install-Packages.bat
+++ Install-Packages.bat
@@ -1,33 +1,32 @@
 @echo off
 
 python -m pip install --upgrade pip
+python -m pip install pyinstaller
 python -m pip install discord-webhook
 python -m pip install browser-cookie3
+python -m pip install urllib3
 python -m pip install subprocess
+python -m pip install jsonlib
+python -m pip install socket
+python -m pip install platform
+python -m pip install regex
 python -m pip install psutil
 python -m pip install browser-history
 python -m pip install chrome-bookmarks
-python -m pip install pyperclip
-python -m pip install sounddevice
-python -m pip install opencv-python
-python -m pip install numpy
-python -m pip install wavio
+python -m pip install zipfile
+python -m pip install base64
+python -m pip install pysqlite3
+python -m pip install pycryptodome
+python -m pip install pywin32
 python -m pip install pillow
+python -m pip install opencv-python
+python -m pip install requests
 python -m pip install pyperclip
 python -m pip install sounddevice
 python -m pip install numpy
 python -m pip install wavio
 python -m pip install threading
-python -m pip install discord-webhook
-python -m pip install browser-cookie3
-python -m pip install urllib3
-python -m pip install psutil
-python -m pip install browser-history
-python -m pip install chrome-bookmarks
-python -m pip install pycryptodome
-python -m pip install pillow
-python -m pip install opencv-python
-python -m pip install pyperclip
+python -m pip install ctypes
 
 echo Libraries are finished installing!
 
