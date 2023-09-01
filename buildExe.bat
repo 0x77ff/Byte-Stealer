e39122ba719599647d343ddc759828dba244206a
TurtlesXD <116833814+TurtlesXD@users.noreply.github.com>
2023-09-01 08:30:32 +0100

    Update buildExe.bat

--- buildExe.bat
+++ buildExe.bat
@@ -5,10 +5,10 @@ set /p "ICON=Enter .ico file path for your exe?(Leave empty for no icon): "
 
 IF EXIST "%USER_INPUT%" (
     if "%ICON%"=="" (
-        call pyinstaller --noconfirm --onefile --windowed --ascii --clean "%USER_INPUT%"
+        call Python -m pyinstaller --noconfirm --onefile --windowed --ascii --clean "%USER_INPUT%"
     ) else (
         IF EXIST "%ICON%" (
-            call pyinstaller --noconfirm --onefile --windowed --icon "%ICON%" --ascii --clean "%USER_INPUT%"
+            call Python -m pyinstaller --noconfirm --onefile --windowed --icon "%ICON%" --ascii --clean "%USER_INPUT%"
         ) ELSE (
             echo "%ICON%" is not a valid path.
         )
