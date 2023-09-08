185833bcf669f8f1b70c75861c940e9bce0f3427
TurtlesXD <116833814+TurtlesXD@users.noreply.github.com>
2023-09-08 17:23:34 +0100

    Update buildExe.bat

--- buildExe.bat
+++ buildExe.bat
@@ -1,20 +1,18 @@
 @echo off
 
-set /p "USER_INPUT=Input Logger.py file path: "
 set /p "ICON=Enter .ico file path for your exe?(Leave empty for no icon): "
 
-IF EXIST "%USER_INPUT%" (
-    if "%ICON%"=="" (
-        call Python -m pyarmor pack --clean -e "--onefile --clean --ascii --noconsole  --icon "%ICON%" " %USER_INPUT%
-    ) else (
-        IF EXIST "%ICON%" (
-            call Python -m pyarmor pack --clean -e "--onefile --clean --ascii --noconsole  --icon "%ICON%" " %USER_INPUT%
-        ) ELSE (
-            echo "%ICON%" is not a valid path.
-        )
+if "%ICON%"=="" (
+    echo pyarmor pack --clean -e "--onefile --noconsole " Logger.py
+    call pyarmor pack --clean -e "--onefile --noconsole " Logger.py
+    pause
+) else (
+    IF EXIST "%ICON%" (
+        echo 
+        call pyarmor pack --clean -e "--onefile --noconsole  --icon " "%ICON%" Logger.py
+        pause
+    ) ELSE (
+        echo "%ICON%" is not a valid path.
     )
-) ELSE (
-    echo "%USER_INPUT%" is not a valid path.
 )
 
-pause
